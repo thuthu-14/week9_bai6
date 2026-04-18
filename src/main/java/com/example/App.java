3f4c4b1f9c5dedc88d2cd0d4a88a99514e0bb4cc
thuthu-14 <thu07114@thu07114gmail.07114gmailcom>
2026-04-18 13:59:03 +0700

    second push

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello CI/CD-second push");
+        System.out.println("Hello CI/CD - second push");
     }
 }
